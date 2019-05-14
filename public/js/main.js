e16a863a077c710585b22f142acebab31506e79d
Nick Austen <nicholas.austen@hitachirail.com>
2019-05-14 22:45:48 +0800

    Stats format as per KTMB; culledunused libs.

--- public/js/main.js
+++ public/js/main.js
@@ -50,22 +50,132 @@ $("#get-btn").on("click", () => {
 });
 // End of addEventListener(click)
 
+processStats = journeyStats => {
+  let data = [];
+  let buckets = [
+    { "0": 0 },
+    { "1": 0 },
+    { "11": 0 },
+    { "16": 0 },
+    { "31": 0 },
+    { "45": 0 }
+  ];
+  console.log("processStats----------------------------");
+
+  let arrDelays = [];
+  let depDelays = [];
+  journeyStats.buckets // Arrivals
+    .slice()
+    .reverse()
+    .forEach((item, idx) => {
+      // Find the bucket for this delay
+      if (delay >= item["45"]) {
+        arrDelays["45"]++;
+      } else if (delay >= item["31"]) {
+        arrDelays["31"]++;
+      } else if (delay >= item["16"]) {
+        arrDelays["16"]++;
+      } else if (delay >= item["11"]) {
+        arrDelays["11"]++;
+      } else if (delay >= item["1"]) {
+        arrDelays["1"]++;
+      } else arrDelays["0"]++;
+    });
+  console.log("RESULT(arrDelays): ", arrDelays);
+  arrDelays.push({});
+  return data;
+};
+
+makeTableHeader = () => {
+  let htmlString = `
+    <table id='my-table'>
+      <thead>
+        <tr>
+          <th rowspan="3">Train<br>No</th>
+          <th rowspan="3">No of<br>Runs</th>
+          <th colspan="6">Departure</th>
+          <th colspan="6">Arrival</th>
+        </tr>
+        <tr>
+          <td rowspan="2">On<br>Time</td>
+          <td colspan="5">Delay in Minutes</td>
+          <td rowspan="2">On<br>Time</td>
+          <td colspan="5">Delay in Minutes</td>
+        </tr>
+        <tr>
+          <td>1-10</td>
+          <td>11-15</td>
+          <td>16-30</td>
+          <td>31-45</td>
+          <td>>45</td>
+          <td>1-10</td>
+          <td>11-15</td>
+          <td>16-30</td>
+          <td>31-45</td>
+          <td>>45</td>
+        </tr>
+      </thead>
+    <tbody>`;
+  // "<table id='my-table' ><thead><th>Num</th><th>Train ID</th><th>Origin</th><th>Date</th><th>Type</th><th>Class</th><th>Events</th><th>Atot</th><th>Aav</th><th>Amx</th><th>Dtot</th><th>Dav</th><th>Dmx</th></thead><tbody>";
+  return htmlString;
+};
 //  r e n d e r H T M L ( )
 //
 renderHTML = data => {
-  var htmlString =
-    "<table id='my-table' ><thead><th>Num</th><th>Train ID</th><th>Origin</th><th>Date</th><th>Type</th><th>Class</th><th>Events</th><th>Atot</th><th>Aav</th><th>Amx</th><th>Dtot</th><th>Dav</th><th>Dmx</th></thead><tbody>";
+  let htmlString = makeTableHeader();
+  // "<table id='my-table' ><thead><th>Num</th><th>Train ID</th><th>Origin</th><th>Date</th><th>Type</th><th>Class</th><th>Events</th><th>Atot</th><th>Aav</th><th>Amx</th><th>Dtot</th><th>Dav</th><th>Dmx</th></thead><tbody>";
 
   if (data.length === 0) {
   } else {
+    // Process arrival and depatrure delays - placing them into buckets...
+    let buckets = [0, 1, 11, 16, 31, 46];
+
+    // Render the dtaa
     data.forEach((item, idx) => {
+      let arrBuckets = [];
+      let depBuckets = [];
+
+      let delay = item.arr.avg;
+      buckets
+        .slice()
+        .reverse()
+        .forEach((i, myIdx) => {
+          arrBuckets[i] = 0;
+          if (delay >= i) {
+            arrBuckets[i]++;
+          }
+        });
+      console.log("arrBuckets", arrBuckets);
+
+      delay = item.dep.avg;
+      buckets
+        .slice()
+        .reverse()
+        .forEach((i, myIdx) => {
+          depBuckets[i] = 0;
+          if (delay >= i) {
+            depBuckets[i]++;
+          }
+        });
+      console.log("depBuckets", depBuckets);
+
       let parts = item.journeyId.split("/");
-      htmlString +=
-        `<tr><td>${idx}</td><td>${parts[0]}</td><td>${parts[1]}</td><td>${
-          parts[2]
-        }</td><td>${item.service}</td><td>${item.category}</td>` +
-        `<td>${item.eventCount}</td><td>${item.arr.sum}</td><td>${item.arr.avg}</td><td>${item.arr.max}</td><td>${item.dep.sum}</td><td>${item.dep.avg}</td><td>${item.dep.max}</td>` +
-        `</tr>`;
+      htmlString += `<tr>
+          <td>${parts[0]}</td>
+          <td>${item.eventCount}</td>
+          <td>${depBuckets[0]}</td>
+          <td>${depBuckets[1]}</td>
+          <td>${depBuckets[11]}</td>
+          <td>${depBuckets[16]}</td>
+          <td>${depBuckets[31]}</td>
+          <td>${depBuckets[46]}</td>
+          <td>${arrBuckets[0]}</td>
+          <td>${arrBuckets[1]}</td>
+          <td>${arrBuckets[11]}</td>
+          <td>${arrBuckets[16]}</td>
+          <td>${arrBuckets[31]}</td>
+          <td>${arrBuckets[46]}</td>
+          </tr>`;
     });
   }
   htmlString += "</tbody></table>";
